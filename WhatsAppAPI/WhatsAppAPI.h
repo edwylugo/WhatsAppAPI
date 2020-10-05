cfbc5c3662a32f79e9065618c86681525daea5ba
Edwy Lugo <edwylugo@gmail.com>
2020-10-05 13:34:07 -0300

    Initial Commit

---
+++ WhatsAppAPI/WhatsAppAPI.h
@@ -0,0 +1,19 @@
+//
+//  WhatsAppAPI.h
+//  WhatsAppAPI
+//
+//  Created by Edwy Lugo on 05/10/20.
+//  Copyright © 2020 Edwy Lugo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WhatsAppAPI.
+FOUNDATION_EXPORT double WhatsAppAPIVersionNumber;
+
+//! Project version string for WhatsAppAPI.
+FOUNDATION_EXPORT const unsigned char WhatsAppAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WhatsAppAPI/PublicHeader.h>
+
+
